88afbbe31cee6ff9e6dd4632d9ab48fa326ace3b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-19 21:34:04 +0200

    Split controllers per application

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -12,7 +12,7 @@
  */
 declare(strict_types=1);
 
-namespace Modules\Tasks;
+namespace Modules\Tasks\Controller;
 
 use Model\Message\FormValidation;
 
@@ -45,197 +45,8 @@ use phpOMS\DataStorage\Database\RelationType;
  * @link       http://website.orange-management.de
  * @since      1.0.0
  */
-final class Controller extends ModuleAbstract implements WebInterface
+class ApiController extends Controller
 {
-
-    /**
-     * Module path.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_PATH = __DIR__;
-
-    /**
-     * Module version.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_VERSION = '1.0.0';
-
-    /**
-     * Module name.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_NAME = 'Tasks';
-
-    /**
-     * Module id.
-     *
-     * @var int
-     * @since 1.0.0
-     */
-    public const MODULE_ID = 1001100000;
-
-    /**
-     * Providing.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $providing = [
-        'Navigation'
-    ];
-
-    /**
-     * Dependencies.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $dependencies = [
-    ];
-
-    /**
-     * @param RequestAbstract  $request  Request
-     * @param ResponseAbstract $response Response
-     * @param mixed            $data     Generic data
-     *
-     * @return \Serializable
-     *
-     * @since  1.0.0
-     * @codeCoverageIgnore
-     */
-    public function viewTaskDashboard(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
-    {
-        $view = new View($this->app, $request, $response);
-
-        $view->setTemplate('/Modules/Tasks/Theme/Backend/task-dashboard');
-        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1001101001, $request, $response));
-
-        $open  = TaskMapper::getOpen($request->getHeader()->getAccount());
-        $tasks = TaskMapper::getNewest(25);
-
-        $view->addData('tasks', $tasks);
-
-        return $view;
-    }
-
-    /**
-     * @param RequestAbstract  $request  Request
-     * @param ResponseAbstract $response Response
-     * @param mixed            $data     Generic data
-     *
-     * @return \Serializable
-     *
-     * @since  1.0.0
-     * @codeCoverageIgnore
-     */
-    public function viewDashboard(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
-    {
-        $view = new View($this->app, $request, $response);
-        $view->setTemplate('/Modules/Tasks/Theme/Backend/dashboard-task');
-        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1001101001, $request, $response));
-
-        $taskListView = new \Modules\Tasks\Theme\Backend\Components\Tasks\BaseView($this->app, $request, $response);
-        $taskListView->setTemplate('/Modules/Tasks/Theme/Backend/Components/Tasks/list');
-        $view->addData('tasklist', $taskListView);
-
-        $tasks = TaskMapper::getNewest(5, null, RelationType::ALL, 1);
-        $view->addData('tasks', $tasks);
-
-        return $view;
-    }
-
-    /**
-     * @param RequestAbstract  $request  Request
-     * @param ResponseAbstract $response Response
-     * @param mixed            $data     Generic data
-     *
-     * @return \Serializable
-     *
-     * @since  1.0.0
-     * @codeCoverageIgnore
-     */
-    public function viewTaskView(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
-    {
-        $view = new TaskView($this->app, $request, $response);
-
-        $task      = TaskMapper::get((int) $request->getData('id'));
-        $accountId = $request->getHeader()->getAccount();
-
-        if (!($task->getCreatedBy()->getId() === $accountId
-            || $task->isCc($accountId)
-            || $task->isForwarded($accountId))
-            && !$this->app->accountManager->get($accountId)->hasPermission(
-                PermissionType::READ, $this->app->orgId, $this->app->appName, self::MODULE_NAME, PermissionState::TASK, $task->getId())
-        ) {
-            $view->setTemplate('/Web/Backend/Error/403_inline');
-            $response->getHeader()->setStatusCode(RequestStatusCode::R_403);
-            return $view;
-        }
-
-        $view->setTemplate('/Modules/Tasks/Theme/Backend/task-single');
-        $view->addData('task', $task);
-        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1001101001, $request, $response));
-
-        $accGrpSelector = new \Modules\Profile\Theme\Backend\Components\AccountGroupSelector\BaseView($this->app, $request, $response);
-        $view->addData('accGrpSelector', $accGrpSelector);
-
-        $editor = new \Modules\Editor\Theme\Backend\Components\Editor\BaseView($this->app, $request, $response);
-        $view->addData('editor', $editor);
-
-        return $view;
-    }
-
-    /**
-     * @param RequestAbstract  $request  Request
-     * @param ResponseAbstract $response Response
-     * @param mixed            $data     Generic data
-     *
-     * @return \Serializable
-     *
-     * @since  1.0.0
-     * @codeCoverageIgnore
-     */
-    public function viewTaskCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
-    {
-        $view = new View($this->app, $request, $response);
-
-        $view->setTemplate('/Modules/Tasks/Theme/Backend/task-create');
-        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1001101001, $request, $response));
-
-        $accGrpSelector = new \Modules\Profile\Theme\Backend\Components\AccountGroupSelector\BaseView($this->app, $request, $response);
-        $view->addData('accGrpSelector', $accGrpSelector);
-
-        $editor = new \Modules\Editor\Theme\Backend\Components\Editor\BaseView($this->app, $request, $response);
-        $view->addData('editor', $editor);
-
-        return $view;
-    }
-
-    /**
-     * @param RequestAbstract  $request  Request
-     * @param ResponseAbstract $response Response
-     * @param mixed            $data     Generic data
-     *
-     * @return \Serializable
-     *
-     * @since  1.0.0
-     * @codeCoverageIgnore
-     */
-    public function viewTaskAnalysis(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
-    {
-        $view = new View($this->app, $request, $response);
-        $view->setTemplate('/Modules/Tasks/Theme/Backend/task-analysis');
-        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1001101001, $request, $response));
-
-        return $view;
-    }
-
     /**
      * Validate task create request
      *
@@ -257,20 +68,6 @@ final class Controller extends ModuleAbstract implements WebInterface
         }
 
         return [];
-    }
-
-    /**
-     * Count unread messages
-     * 
-     * @param int $account Account id
-     *
-     * @return int
-     *
-     * @since  1.0.0
-     */
-    public function openNav(int $account) : int
-    {
-        return TaskMapper::countUnread($account);
     }
 
     /**
