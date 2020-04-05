06a9c943890e44f464b23b1711414ecac8728c9c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-04-05 19:31:56 +0200

    add/improve locale

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Tasks
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Tasks' => [
+    'Account'            => 'Benutzer',
+    'All'                => 'Alle',
+    'AverageAmount'      => 'ØAnyahl',
+    'AverageProcessTime' => 'ØBearbeitungszeit',
+    'BCC'                => 'BCC',
+    'By'                 => 'Von',
+    'CC'                 => 'CC',
+    'Created'            => 'Erstellt',
+    'Creator'            => 'Ersteller',
+    'Day'                => 'Tag',
+    'Default'            => 'Standard',
+    'Due'                => 'Fällig',
+    'Due/Priority'       => 'Fällig/Priorität',
+    'Forward'            => 'Weiterleiten',
+    'Forwarded'          => 'Weitergeleitet',
+    'ForwardedTo'        => 'Weiterleiten an',
+    'From'               => 'Von',
+    'Group'              => 'Gruppe',
+    'History'            => 'Übersicht',
+    'Interval'           => 'Intervall',
+    'InTime'             => 'In Zeit',
+    'Media'              => 'Media',
+    'Message'            => 'Nachricht',
+    'Month'              => 'Monat',
+    'Name'               => 'Name',
+    'New'                => 'Neu',
+    'Open'               => 'Offen',
+    'P0'                 => 'Nichts',
+    'P1'                 => 'Sehr Niedrig',
+    'P2'                 => 'Niedrig',
+    'P3'                 => 'Medium',
+    'P4'                 => 'Hoch',
+    'P5'                 => 'Sehr Hoch',
+    'Person'             => 'Person',
+    'Priority'           => 'Priorität',
+    'Received'           => 'Erhalten',
+    'Redirected'         => 'Weitergeleitet',
+    'S1'                 => 'Offen',
+    'S2'                 => 'In Bearbeitung',
+    'S3'                 => 'Unterbrochen',
+    'S4'                 => 'Abgebrochen',
+    'S5'                 => 'Fertig',
+    'S6'                 => 'Geschlossen',
+    'Select'             => 'Auswahl',
+    'Settings'           => 'Einstellung',
+    'SharedVisibility'   => 'Sichtbarkeit ist mit allen geteilt',
+    'Size'               => 'Größe',
+    'Statistics'         => 'Statistiken',
+    'Status'             => 'Status',
+    'Task'               => 'Task',
+    'Tasks'              => 'Tasks',
+    'Template'           => 'Template',
+    'Time'               => 'Zeit',
+    'Title'              => 'Titel',
+    'To'                 => 'An',
+    'Today'              => 'Heute',
+    'Type'               => 'Typ',
+    'Upload'             => 'Upload',
+    'Week'               => 'Woche',
+    'Year'               => 'Jahr',
+]];
