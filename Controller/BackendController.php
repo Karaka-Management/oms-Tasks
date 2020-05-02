bda4d7efe3db4dcf1bfebab2ae8735c6e6999061
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-05-02 10:13:21 +0200

    improve docblocks for Mapper:: variables

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -76,10 +76,15 @@ final class BackendController extends Controller implements DashboardElementInte
                 TaskMapper::withConditional('language', $response->getHeader()->getL11n()->getLanguage())
                     ::getBeforePivot((int) ($request->getData('id') ?? 0), null, 25)
             );
-        } else {
+        } elseif ($request->getData('ptype') === '+') {
             $view->setData('tasks',
                 TaskMapper::withConditional('language', $response->getHeader()->getL11n()->getLanguage())
                     ::getAfterPivot((int) ($request->getData('id') ?? 0), null, 25)
+            );
+        } else {
+            $view->setData('tasks',
+                TaskMapper::withConditional('language', $response->getHeader()->getL11n()->getLanguage())
+                    ::getAfterPivot(0, null, 25)
             );
         }
 
