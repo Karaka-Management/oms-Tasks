c3be3368ce0e699382332466ed214dc409b6cb07
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-11-28 19:26:40 +0100

    fix tests

--- tests/BackendControllerTest.php
+++ tests/BackendControllerTest.php
@@ -46,7 +46,7 @@ class BackendControllerTest extends \PHPUnit\Framework\TestCase
         $this->app->orgId          = 1;
         $this->app->accountManager = new AccountManager($GLOBALS['session']);
         $this->app->appSettings    = new CoreSettings($this->app->dbPool->get());
-        $this->app->moduleManager  = new ModuleManager($this->app, __DIR__ . '/../../../Modules');
+        $this->app->moduleManager  = new ModuleManager($this->app, __DIR__ . '/../../../Modules/');
         $this->app->dispatcher     = new Dispatcher($this->app);
         $this->app->eventManager   = new EventManager($this->app->dispatcher);
         $this->app->eventManager->importFromFile(__DIR__ . '/../../../Web/Backend/Hooks.php');
