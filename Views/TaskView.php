772fc7963abba28c3fc44e9615bc933023d0fb61
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-11-29 20:08:14 +0100

    fix bugs

--- Views/TaskView.php
+++ Views/TaskView.php
@@ -17,7 +17,11 @@ namespace Modules\Tasks\Views;
 use Modules\Media\Models\Media;
 use Modules\Media\Models\NullMedia;
 use Modules\Profile\Models\ProfileMapper;
+use Modules\Profile\Models\NullProfile;
 use Modules\Tasks\Models\TaskStatus;
+use phpOMS\Localization\L11nManager;
+use phpOMS\Message\RequestAbstract;
+use phpOMS\Message\ResponseAbstract;
 use phpOMS\Uri\UriFactory;
 use phpOMS\Views\View;
 
@@ -44,9 +48,10 @@ class TaskView extends View
      *
      * @since 1.0.0
      */
-    public function __construct()
+    public function __construct(L11nManager $l11n = null, RequestAbstract $request = null, ResponseAbstract $response = null)
     {
         $this->defaultProfileImage = new NullMedia();
+        parent::__construct($l11n, $request, $response);
     }
 
     /**
