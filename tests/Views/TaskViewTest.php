dc436833f5050d349389ef5dbfc7660470d028e5
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-24 22:44:33 +0000

    test fixes

--- tests/Views/TaskViewTest.php
+++ tests/Views/TaskViewTest.php
@@ -14,6 +14,11 @@ declare(strict_types=1);
 
 namespace Modules\Tasks\tests\Views;
 
+use Modules\Admin\Models\AccountMapper;
+use Modules\Admin\Models\NullAccount;
+use Modules\Media\Models\Media;
+use Modules\Profile\Models\Profile;
+use Modules\Profile\Models\ProfileMapper;
 use Modules\Tasks\Views\TaskView;
 
 /**
@@ -38,8 +43,32 @@ class TaskViewTest extends \PHPUnit\Framework\TestCase
      */
     public function testAccountImageUrl() : void
     {
-        $view = new TaskView();
+        $media              = new Media();
+        $media->createdBy   = new NullAccount(1);
+        $media->description = 'desc';
+        $media->setPath('Web/Backend/img/default-user.jpg');
+        $media->size      = 11;
+        $media->extension = 'png';
+        $media->name      = 'Image';
 
+        if (($profile = ProfileMapper::get()->where('account', 1)->execute())->id === 0) {
+            $profile = new Profile();
+
+            $profile->account  = AccountMapper::get()->where('id', 1)->execute();
+            $profile->image    = $media;
+            $profile->birthday =  new \DateTime('now');
+
+            $id = ProfileMapper::create()->execute($profile);
+            self::assertGreaterThan(0, $profile->id);
+            self::assertEquals($id, $profile->id);
+        } else {
+            $profile->image    = $media;
+            $profile->birthday =  new \DateTime('now');
+
+            ProfileMapper::update()->with('image')->execute($profile);
+        }
+
+        $view = new TaskView();
         self::assertEquals('Web/Backend/img/default-user.jpg', $view->getAccountImage(1));
     }
 }
