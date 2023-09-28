ffcc3ee79552d400b8d19dd82ba5eb5991df66a5
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-28 01:04:16 +0000

    fix tests

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -301,7 +301,7 @@ final class ApiController extends Controller
                     $request->setData('language', $tag['language'], true);
 
                     $internalResponse = new HttpResponse();
-                    $this->app->moduleManager->get('Tag')->apiTagCreate($request, $internalResponse, null);
+                    $this->app->moduleManager->get('Tag')->apiTagCreate($request, $internalResponse);
 
                     if (!\is_array($data = $internalResponse->get($request->uri->__toString()))) {
                         continue;
