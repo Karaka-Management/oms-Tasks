367516e5b222662bd4420d90ae85e3cb6620ff9d
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2025-03-11 18:39:42 +0000

    fix bugs

--- Theme/Backend/dashboard-task.tpl.php
+++ Theme/Backend/dashboard-task.tpl.php
@@ -17,7 +17,6 @@ use phpOMS\Uri\UriFactory;
 
 $tasksList = $this->data['tasks'] ?? [];
 ?>
-
 <div id="tasks-dashboard" class="col-xs-12 col-md-6" draggable="true">
     <section class="portlet">
         <div class="portlet-head"><?= $this->getHtml('Tasks', 'Tasks'); ?></div>
